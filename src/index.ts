21d106bd9b2182a4b9a3cdc72dac3c9645c95164
EricD68 <eric.deaville@gmail.com>
2025-11-09 15:03:13 +0000

    Move ort-single import to the top of index.ts
    Ensure ort-single is the first import in the bundle.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import "./ort-single";        // << MUST be first import in the bundle
+// (rest of their original imports follow)
+
 export * from './inference';
 export * from './storage';
 export * from './voices';
